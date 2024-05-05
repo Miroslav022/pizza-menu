45708c5abf9c1630b81bfaafb9d9b5ba846b8ab1
Miroslav022 <mikipc440@gmail.com>
2024-05-05 17:59:55 +0200

    First commit

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 7;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          {" "}
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
+    </footer>
+  );
+}
+
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
+      </p>
+      <button className="btn">order</button>
+    </div>
+  );
+}
+export default Footer;
